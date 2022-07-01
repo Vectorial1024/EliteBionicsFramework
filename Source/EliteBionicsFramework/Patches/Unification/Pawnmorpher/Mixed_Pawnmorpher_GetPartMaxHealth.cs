6bf165126197e7fcfb2ff6b7ca8cb0e0eca68ff0
Vincent Wong <vincentwwy168@gmail.com>
2022-07-01 21:53:58 +0800

    Clean up Pawnmorpher patches a second time

--- Source/EliteBionicsFramework/Patches/Unification/Pawnmorpher/Mixed_Pawnmorpher_GetPartMaxHealth.cs
+++ Source/EliteBionicsFramework/Patches/Unification/Pawnmorpher/Mixed_Pawnmorpher_GetPartMaxHealth.cs
@@ -1,11 +1,7 @@
 ﻿using EBF.Util;
 using HarmonyLib;
 using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Reflection;
-using System.Text;
-using System.Threading.Tasks;
 using Verse;
 
 namespace EBF.Patches.Unification.Pawnmorpher
