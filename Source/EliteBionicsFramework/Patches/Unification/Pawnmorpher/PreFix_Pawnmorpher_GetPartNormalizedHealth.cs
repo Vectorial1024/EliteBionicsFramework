8197ccef94253347c015014539546b667687e71d
Vincent Wong <vincentwwy168@gmail.com>
2023-01-01 16:01:18 +0800

    Suppress Pawnmorpher get-max-health warning

---
+++ Source/EliteBionicsFramework/Patches/Unification/Pawnmorpher/PreFix_Pawnmorpher_GetPartNormalizedHealth.cs
@@ -0,0 +1,30 @@
+﻿using EBF.Util;
+using HarmonyLib;
+using System;
+using System.Reflection;
+using Verse;
+
+namespace EBF.Patches.Unification.Pawnmorpher
+{
+    [HarmonyPatch]
+    public class PreFix_Pawnmorpher_GetPartNormalizedHealth
+    {
+        public static bool Prepare()
+        {
+            return ModDetector.PawnmorpherIsLoaded;
+        }
+
+        public static MethodBase TargetMethod()
+        {
+            return AccessTools.Method("Pawnmorph.BodyUtilities:GetPartNormalizedHealth");
+        }
+
+        [HarmonyPrefix]
+        public static bool ManuallyPatchTheMethod(ref float __result, BodyPartRecord record, Pawn p)
+        {
+            // tell the relevant side to ignore the next getMaxHealth warning
+            Prefix_BodyPart_GetMaxHealth.SuppressNextWarning();
+            return true;
+        }
+    }
+}
