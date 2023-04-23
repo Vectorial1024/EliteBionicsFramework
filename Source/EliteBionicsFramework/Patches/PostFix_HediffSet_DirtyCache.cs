26ce9eab792927633dc01068b2415cae26fb5998
Vincent Wong <vincentwwy168@gmail.com>
2023-04-23 22:58:03 +0800

    Allow to selectively ignore dirty-health-cache calls

--- Source/EliteBionicsFramework/Patches/PostFix_HediffSet_DirtyCache.cs
+++ Source/EliteBionicsFramework/Patches/PostFix_HediffSet_DirtyCache.cs
@@ -1,5 +1,7 @@
 ﻿using EBF.Util;
 using HarmonyLib;
+using System;
+using System.Collections.Generic;
 using Verse;
 
 namespace EBF.Patches
@@ -8,11 +10,30 @@ namespace EBF.Patches
     [HarmonyPatch("DirtyCache", MethodType.Normal)]
     public class PostFix_HediffSet_DirtyCache
     {
+        [ThreadStatic]
+        internal static HashSet<Pawn> dirtyCacheIgnoreSet = new HashSet<Pawn>();
+
+        /// <summary>
+        /// Suppresses the next "EBF Protocol violation" error message.
+        /// Because RimWorld is only single-threaded, we can do crazy things like this.
+        /// </summary>
+        internal static void SuppressNextDirtyCache(Pawn target)
+        {
+            dirtyCacheIgnoreSet.Add(target);
+        }
+
         [HarmonyPostfix]
         public static void PostFix(HediffSet __instance)
         {
             // convenient place to keep track of hediff/alive changes
-            MaxHealthCache.ResetCacheForPawn(__instance.pawn);
+            Pawn pawn = __instance.pawn;
+            if (dirtyCacheIgnoreSet.Contains(pawn))
+            {
+                // but dont do it if we are told to ignore it
+                dirtyCacheIgnoreSet.Remove(pawn);
+                return;
+            }
+            MaxHealthCache.ResetCacheForPawn(pawn);
         }
     }
 }
---
+++ Source/EliteBionicsFramework/Patches/Prefix_HealthTracker_NotifyHediffChanged.cs
@@ -0,0 +1,23 @@
+﻿using HarmonyLib;
+using Verse;
+
+namespace EBF.Patches
+{
+
+    [HarmonyPatch(typeof(Pawn_HealthTracker))]
+    [HarmonyPatch("Notify_HediffChanged", MethodType.Normal)]
+    public class Prefix_HealthTracker_NotifyHediffChanged
+    {
+        [HarmonyPrefix]
+        public static void PreFix(Pawn ___pawn, Hediff hediff)
+        {
+            if (hediff is Hediff_Injury)
+            {
+                // healing injuries will very often tick the health cache, but it is irrelevant to our operations
+                // therefore, ignore the next dirty cache call
+                Log.Error("Ricking Hediff_Injury for pawn " + ___pawn.ToStringSafe() + "; skipping it.");
+                PostFix_HediffSet_DirtyCache.SuppressNextDirtyCache(___pawn);
+            }
+        }
+    }
+}
