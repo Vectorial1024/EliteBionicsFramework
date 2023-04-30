3584c0820bd26bcb8fc496730daf37f3c896ae82
Vincent Wong <vincentwwy168@gmail.com>
2023-04-30 14:16:41 +0800

    Reset max HP cache when Pawnmorpher has state changes

---
+++ Source/EliteBionicsFramework/Patches/Unification/Pawnmorpher/PostFix_Pawnmorpher_HediffStageChanges_Recache.cs
@@ -0,0 +1,28 @@
+﻿using EBF.Util;
+using HarmonyLib;
+using System.Reflection;
+using Verse;
+
+namespace EBF.Patches.Unification.Pawnmorpher
+{
+    [HarmonyPatch]
+    public class PostFix_Pawnmorpher_HediffStageChanges_Recache
+    {
+        public static bool Prepare()
+        {
+            return ModDetector.PawnmorpherIsLoaded;
+        }
+
+        public static MethodBase TargetMethod()
+        {
+            return AccessTools.Method("Pawnmorph.Hediffs.Hediff_StageChanges:RecacheStage");
+        }
+
+        [HarmonyPostfix]
+        public static void NotifyRecalculateHealth(Hediff __instance)
+        {
+            // state changed; recalculate the health!
+            MaxHealthCache.ResetCacheForPawn(__instance.pawn);
+        }
+    }
+}
