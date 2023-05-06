3adb08555400c589f2f43d91639ea357d0a055d9
Vincent Wong <vincentwwy168@gmail.com>
2023-05-06 22:09:05 +0800

    Remove caches when pawns die, etc
    Will also trigger when corpses are removed

---
+++ Source/EliteBionicsFramework/Patches/PostFix_Corpse_PostCorpseDestroy.cs
@@ -0,0 +1,17 @@
+﻿using EBF.Util;
+using HarmonyLib;
+using Verse;
+
+namespace EBF.Patches
+{
+    [HarmonyPatch(typeof(Corpse))]
+    [HarmonyPatch("PostCorpseDestroy", MethodType.Normal)]
+    public class PostFix_Corpse_PostCorpseDestroy
+    {
+        [HarmonyPostfix]
+        public static void OnCorpseDestroyed(Pawn pawn)
+        {
+            MaxHealthCache.ResetCacheForPawn(pawn);
+        }
+    }
+}
---
+++ Source/EliteBionicsFramework/Patches/PostFix_WorldPawns_PawnDestroyed.cs
@@ -0,0 +1,18 @@
+﻿using EBF.Util;
+using HarmonyLib;
+using RimWorld.Planet;
+using Verse;
+
+namespace EBF.Patches
+{
+    [HarmonyPatch(typeof(WorldPawns))]
+    [HarmonyPatch("Notify_PawnDestroyed", MethodType.Normal)]
+    public class PostFix_WorldPawns_PawnDestroyed
+    {
+        [HarmonyPostfix]
+        public static void OnPawnDestroyed(Pawn p)
+        {
+            MaxHealthCache.ResetCacheForPawn(p);
+        }
+    }
+}
