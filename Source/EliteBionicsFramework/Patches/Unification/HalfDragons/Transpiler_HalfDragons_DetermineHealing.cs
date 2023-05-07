9a61d991864c7828713aec56042530edfef433d6
Vincent Wong <vincentwwy168@gmail.com>
2023-05-07 12:53:40 +0800

    Fix the misleading patch file contents

--- Source/EliteBionicsFramework/Patches/Unification/HalfDragons/Transpiler_HalfDragons_DetermineHealing.cs
+++ Source/EliteBionicsFramework/Patches/Unification/HalfDragons/Transpiler_HalfDragons_DetermineHealing.cs
@@ -1,8 +1,11 @@
 ﻿using EBF.Util;
 using HarmonyLib;
+using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Reflection;
 using System.Reflection.Emit;
+using Verse;
 
 namespace EBF.Patches.Unification.HalfDragons
 {
@@ -11,12 +14,50 @@ namespace EBF.Patches.Unification.HalfDragons
     {
         public static bool Prepare()
         {
-            return ModDetector.HalfDragonsIsLoaded;
+            return TargetMethod() != null;
         }
 
         public static MethodBase TargetMethod()
         {
-            return AccessTools.Method("HalfDragons.Need_DragonBlood.RemoveHealedParts");
+            if (!ModDetector.HalfDragonsIsLoaded)
+            {
+                return null;
+            }
+            /*
+             * Oh no, the terrors of self-anon...
+             * Refer to documentation elsewhere for an explanation of self-anon.
+             * Just search "selfanon" and you should get something.
+             * 
+             * This time, wa are patching a method inside the self-anon class.
+             * This reminds me of a certain RimWorld mod that I have abandoned because it involves patching some values inside self-anon types...
+             */
+            Type typeSelfAnon = AccessTools.TypeByName("HalfDragons.Need_DragonBlood").GetNestedTypes(BindingFlags.NonPublic).Where((Type type) => type.GetField("hediffs") != null).First();
+            if (typeSelfAnon == null)
+            {
+                return null;
+            }
+            // it should exist, but it has no known name
+            // match the signature of function (BodyPartRecord)
+            MethodInfo theMethod = null;
+            foreach (MethodInfo method in typeSelfAnon.GetMethods())
+            {
+                foreach (ParameterInfo parameter in method.GetParameters())
+                {
+                    // it should only have 1 param
+                    if (parameter.ParameterType == typeof(BodyPartRecord))
+                    {
+                        // is this one
+                        theMethod = method;
+                    }
+                    break;
+                }
+                if (theMethod != null)
+                {
+                    break;
+                }
+            }
+            // we should have matched the method...
+            return theMethod;
         }
 
         public static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions)
@@ -26,6 +67,7 @@ namespace EBF.Patches.Unification.HalfDragons
             short suppressCount = 0;
             bool patchComplete = false;
 
+            // we are here only if we have found the method
             foreach (CodeInstruction instruction in instructions)
             {
                 if (!patchComplete && instruction.opcode == OpCodes.Callvirt)
@@ -34,7 +76,7 @@ namespace EBF.Patches.Unification.HalfDragons
 
                     if (occurencesCallvirt == 2)
                     {
-                        yield return new CodeInstruction(OpCodes.Ldloc_1);
+                        yield return new CodeInstruction(OpCodes.Ldarg_1);
                         yield return new CodeInstruction(OpCodes.Call, typeof(VanillaExtender).GetMethod("GetMaxHealth_Cached"));
 
                         suppressCount = 1;
