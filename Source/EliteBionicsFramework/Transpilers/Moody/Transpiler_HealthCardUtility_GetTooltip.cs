764430ba40c6f13297c79c3c67db4d1100d3e9cd
Vincent Wong <vincentwwy168@gmail.com>
2023-05-31 19:30:40 +0800

    Replace Moody transpiler with CodeMatcher

--- Source/EliteBionicsFramework/Transpilers/Moody/Transpiler_HealthCardUtility_GetTooltip.cs
+++ Source/EliteBionicsFramework/Transpilers/Moody/Transpiler_HealthCardUtility_GetTooltip.cs
@@ -25,36 +25,22 @@ namespace EBF.Transpilations.Moody
 
         public static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions)
         {
-            // Patch things up at the 13th occurence of callvirt
-            short occurencesCallvirt = 0;
-            short suppressCount = 0;
-            bool patchComplete = false;
-
-            foreach (CodeInstruction instruction in instructions)
-            {
-                if (!patchComplete && instruction.opcode == OpCodes.Callvirt)
-                {
-                    occurencesCallvirt++;
-
-                    if (occurencesCallvirt == 13)
-                    {
-                        yield return new CodeInstruction(OpCodes.Ldloc_3);
-                        yield return new CodeInstruction(OpCodes.Callvirt, typeof(Hediff).GetProperty("Part").GetGetMethod());
-                        yield return new CodeInstruction(OpCodes.Call, typeof(VanillaExtender).GetMethod("GetMaxHealth_Cached"));
-
-                        suppressCount = 1;
-                        patchComplete = true;
-                    }
-                }
-
-                if (suppressCount > 0)
-                {
-                    instruction.opcode = OpCodes.Nop;
-                    suppressCount--;
-                }
-
-                yield return instruction;
-            }
+            /*
+             * A total of 1 GetMaxHealth occurences detected;
+             * Patch with CodeMatcher
+             */
+            return new CodeMatcher(instructions)
+                .MatchStartForward(
+                    new CodeMatch(OpCodes.Callvirt, AccessTools.Method(typeof(BodyPartDef), nameof(BodyPartDef.GetMaxHealth)))
+                ) // find the only occurence of .GetMaxHealth()
+                .InsertAndAdvance(
+                    new CodeInstruction(OpCodes.Ldloc_3),
+                    new CodeInstruction(OpCodes.Callvirt, typeof(Hediff).GetProperty("Part").GetGetMethod()),
+                    new CodeInstruction(OpCodes.Call, typeof(VanillaExtender).GetMethod("GetMaxHealth"))
+                ) // insert extra code so that we use VanillaExtender.GetMaxHealth(); we do this out of convenience
+                .Set(OpCodes.Nop, null)
+                // and ignore the original instruction
+                .InstructionEnumeration();
         }
     }
 }
