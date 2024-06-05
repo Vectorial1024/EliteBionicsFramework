7f831e7dbfa93a4c6f059c67bb1a00c96c7846bc
Vincent Wong <vincentwwy168@gmail.com>
2024-06-06 01:04:09 +0800

    Create util struct for future refactoring

---
+++ Source/EliteBionicsFramework/Util/VerbAttackInfo.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Verse;
+
+namespace EBF.Util
+{
+    /// <summary>
+    /// A struct for storing the verb attack info of a (melee) attack, for convenient identification and calculation.
+    /// </summary>
+    /// <param name="tool"></param>
+    /// <param name="source"></param>
+    public readonly struct VerbAttackInfo(Tool tool, HediffComp_VerbGiver source) : IEquatable<VerbAttackInfo>
+    {
+        public readonly Tool tool = tool;
+        public readonly HediffComp_VerbGiver source = source;
+
+        public bool IsValid
+        {
+            get
+            {
+                // the tool must exist, but the verbgiver source can be optional (when given by e.g. genes, special hediffs, etc. etc.)
+                return tool != null;
+            }
+        }
+
+        public bool Equals(VerbAttackInfo other)
+        {
+            return tool == other.tool && source == other.source;
+        }
+    }
+}
