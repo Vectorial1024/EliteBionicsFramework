34a1797eaa8f1ac97f829e9d298fd9a6ca974393
Vincent Wong <vincentwwy168@gmail.com>
2024-06-09 17:05:06 +0800

    Clean up

--- Source/EliteBionicsFramework/Patches/Mixed_VerbProperties_AdjustedMeleeDamageAmount.cs
+++ Source/EliteBionicsFramework/Patches/Mixed_VerbProperties_AdjustedMeleeDamageAmount.cs
@@ -1,10 +1,6 @@
 ﻿using EBF.Util;
 using HarmonyLib;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using Verse;
 
 namespace EBF.Patches
--- Source/EliteBionicsFramework/Util/VerbAttackInfo.cs
+++ Source/EliteBionicsFramework/Util/VerbAttackInfo.cs
@@ -1,9 +1,6 @@
 ﻿using EBF.Hediffs;
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using Verse;
 
 namespace EBF.Util
