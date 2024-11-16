e70eb22133934c6665b2ded89a280e25626237dc
Vincent Wong <vincentwwy168@gmail.com>
2024-11-17 00:39:29 +0800

    Add compatibility with BetterPawnInfo

---
+++ Source/EliteBionicsFramework/Transpilers/BetterInfoCard/Transpiler_BIC_StatWorker_BodyPart.cs
@@ -0,0 +1,41 @@
+﻿using EBF.Util;
+using HarmonyLib;
+using System.Collections.Generic;
+using System.Reflection.Emit;
+using System.Reflection;
+using Verse;
+
+namespace EBF.Transpilers.BetterInfoCard
+{
+    [HarmonyPatch]
+    public class Transpiler_BIC_StatWorker_BodyPart
+    {
+        public static bool Prepare()
+        {
+            return ModDetector.BetterInfoCardIsLoaded;
+        }
+
+        public static MethodBase TargetMethod()
+        {
+            return AccessTools.Method("BetterInfoCard.StatWorker_BodyPart:GetBodyPartStr");
+        }
+
+        public static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions)
+        {
+            /*
+             * A total of 1 GetMaxHealth occurences detected;
+             * Patch with CodeMatcher
+             */
+            return new CodeMatcher(instructions)
+                .MatchStartForward(
+                    new CodeMatch(OpCodes.Callvirt, AccessTools.Method(typeof(BodyPartDef), nameof(BodyPartDef.GetMaxHealth)))
+                ) // find the only occurence of .GetMaxHealth()
+                .InsertAndAdvance(
+                    new CodeInstruction(OpCodes.Ldarg_1),
+                    new CodeInstruction(OpCodes.Call, VanillaExtender.ReflectionGetMaxHealth())
+                ) // insert extra code so that we use VanillaExtender.GetMaxHealth(); we do this out of convenience
+                .Set(OpCodes.Nop, null) // and ignore the original instruction
+                .InstructionEnumeration();
+        }
+    }
+}
