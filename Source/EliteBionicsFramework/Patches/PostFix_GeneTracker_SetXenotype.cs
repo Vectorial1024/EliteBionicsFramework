6ef3d2f2ac2c63caae393233707c4c1bb002c601
Vincent Wong <vincentwwy168@gmail.com>
2025-03-31 15:59:16 +0800

    Begin tracking Xenotype changes

---
+++ Source/EliteBionicsFramework/Patches/PostFix_GeneTracker_SetXenotype.cs
@@ -0,0 +1,20 @@
+﻿using EBF.Util;
+using HarmonyLib;
+using RimWorld;
+using Verse;
+
+namespace EBF.Patches
+{
+    [HarmonyPatch(typeof(Pawn_GeneTracker))]
+    [HarmonyPatch(nameof(Pawn_GeneTracker.SetXenotype), MethodType.Normal)]
+    public class PostFix_GeneTracker_SetXenotype
+    {
+        [HarmonyPostfix]
+        public static void ResetCacheForPawn(Pawn ___pawn)
+        {
+            // xenotypes affect the entire pawn, and are somewhat similar to whole-body hediffs
+            MaxHealthCache.ResetCacheForPawn(___pawn);
+            ToolPowerInfoCache.ResetCacheForPawn(___pawn);
+        }
+    }
+}
---
+++ Source/EliteBionicsFramework/Patches/PostFix_GeneTracker_SetXenotypeDirect.cs
@@ -0,0 +1,20 @@
+﻿using EBF.Util;
+using HarmonyLib;
+using RimWorld;
+using Verse;
+
+namespace EBF.Patches
+{
+    [HarmonyPatch(typeof(Pawn_GeneTracker))]
+    [HarmonyPatch(nameof(Pawn_GeneTracker.SetXenotypeDirect), MethodType.Normal)]
+    public class PostFix_GeneTracker_SetXenotypeDirect
+    {
+        [HarmonyPostfix]
+        public static void ResetCacheForPawn(Pawn ___pawn)
+        {
+            // xenotypes affect the entire pawn, and are somewhat similar to whole-body hediffs
+            MaxHealthCache.ResetCacheForPawn(___pawn);
+            ToolPowerInfoCache.ResetCacheForPawn(___pawn);
+        }
+    }
+}
