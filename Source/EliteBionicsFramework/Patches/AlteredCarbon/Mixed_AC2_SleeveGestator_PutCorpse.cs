7a7292044b9fc1cfa2aeda2a7a09537570401430
Vincent Wong <vincentwwy168@gmail.com>
2025-03-31 16:45:59 +0800

    Use another approach to handle AC2 compatibility
    This should finally fix it...

---
+++ Source/EliteBionicsFramework/Patches/AlteredCarbon/Mixed_AC2_SleeveGestator_PutCorpse.cs
@@ -0,0 +1,38 @@
+﻿using EBF.Util;
+using HarmonyLib;
+using System.Reflection;
+
+namespace EBF.Patches.AlteredCarbon
+{
+    [HarmonyPatch]
+    public class Mixed_AC2_SleeveGestator_PutCorpse
+    {
+        /*
+         * Note to self:
+         * This case is very difficult to patch in the usual way because the compiler essentially in-lined the lambda function call into some slippery byte code.
+         * Luckily, because this case involves only Hediff_MissingPart, EBF has no actual need to be active here.
+         * Therefore, we are using another approach: simply suppress EBF and call it a day.
+         */
+
+        public static bool Prepare()
+        {
+            return ModDetector.AlteredCarbon2IsLoaded;
+        }
+
+        public static MethodBase TargetMethod()
+        {
+            return AccessTools.Method("AlteredCarbon.Building_SleeveGestator:PutCorpseForRepurposing");
+        }
+
+        [HarmonyPrefix]
+        public static void BeginIgnoreEbf()
+        {
+            PostFix_BodyPart_GetMaxHealth.TurnOffWarning();
+        }
+
+        public static void EndIgnoreEbf()
+        {
+            PostFix_BodyPart_GetMaxHealth.TurnOnWarning();
+        }
+    }
+}
--- Source/EliteBionicsFramework/Patches/PostFix_BodyPart_GetMaxHealth.cs
+++ Source/EliteBionicsFramework/Patches/PostFix_BodyPart_GetMaxHealth.cs
@@ -13,6 +13,8 @@ namespace EBF.Patches
 
         internal static bool shouldSupressNextWarning = false;
 
+        internal static bool warningIsTurnedOff = false;
+
         /// <summary>
         /// Suppresses the next "EBF Protocol violation" error message.
         /// Because RimWorld is only single-threaded, we can do crazy things like this.
@@ -22,9 +24,33 @@ namespace EBF.Patches
             shouldSupressNextWarning = true;
         }
 
+        /// <summary>
+        /// Temporarily turns off the "EBF Protocol violation" error message until it is turned back on.
+        /// Avoid using this way of error suppression where possible.
+        /// </summary>
+        internal static void TurnOffWarning()
+        {
+            warningIsTurnedOff = true;
+        }
+
+        /// <summary>
+        /// Resumes normal "EBF Protocol violation" error message.
+        /// Avoid using this way of error suppression where possible.
+        /// </summary>
+        internal static void TurnOnWarning()
+        {
+            warningIsTurnedOff = false;
+        }
+
         [HarmonyPostfix]
         public static void CheckEbfProtocolViolation(BodyPartDef __instance, ref float __result, Pawn pawn)
         {
+            if (warningIsTurnedOff)
+            {
+                // sometimes, we just need the system to shut up and trust the process...
+                return;
+            }
+
             // change of strategy: if we are no longer preventing the vanilla usage, might as well do it in the post-fix.
             StackFrame investigateFrame = new StackFrame(2);
             string namespaceString = investigateFrame.GetMethod().ReflectedType.Namespace;
