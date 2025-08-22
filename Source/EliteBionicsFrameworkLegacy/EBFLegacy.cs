a7593a9fc382866ac405cf9b454330e66746925a
Vincent Wong <vincentwwy168@gmail.com>
2025-08-22 13:05:25 +0800

    Set up version detector

--- Source/EliteBionicsFrameworkLegacy/EBFLegacy.cs
+++ Source/EliteBionicsFrameworkLegacy/EBFLegacy.cs
@@ -11,12 +11,30 @@ namespace EliteBionicsFrameworkLegacy
     // note: this mod is only a util mod that deals with legacy EBF versions. Ideally outside modders do not need to interact with this mod.
     public class EBFLegacy : Mod
     {
+        public static string MODSHORTID => "V1024-EBF-L";
+
         public EBFLegacy(ModContentPack content) : base(content)
         {
+            LogInfo("Elite Bionics Framework Legacy Manager starting up with RimWorld version as " + RimWorldDetector.RimWorldVersion);
             Harmony harmony = new Harmony("rimworld." + content.PackageId + ".legacy");
             harmony.PatchAll();
-            Version rimworldVersion = typeof(Mod).Assembly.GetName().Version;
-            Log.Error("EYYY");
+        }
+
+        public static string MODPREFIX => "[" + MODSHORTID + "]";
+
+        public static void LogError(string message)
+        {
+            Log.Error(MODPREFIX + " " + message);
+        }
+
+        public static void LogWarning(string message)
+        {
+            Log.Warning(MODPREFIX + " " + message);
+        }
+
+        public static void LogInfo(string message)
+        {
+            Log.Message(MODPREFIX + " " + message);
         }
     }
 }
---
+++ Source/EliteBionicsFrameworkLegacy/RimWorldDetector.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Verse;
+
+namespace EliteBionicsFrameworkLegacy
+{
+    internal class RimWorldDetector
+    {
+        private static Version rimWorldVersion = null;
+
+        public static Version RimWorldVersion
+        {
+            get
+            {
+                if (rimWorldVersion == null)
+                {
+                    rimWorldVersion = typeof(Mod).Assembly.GetName().Version;
+                }
+                return rimWorldVersion;
+            }
+        }
+    }
+}
