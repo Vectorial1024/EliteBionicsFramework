ca00c364587e97428ea2707668c782e0adb2dbde
Vincent Wong <vincentwwy168@gmail.com>
2025-08-22 16:21:52 +0800

    Do not detect Mlie's variant of Callouts

---
+++ Source/EliteBionicsFrameworkLegacy/Patches/EBF_CalloutsRedirect.cs
@@ -0,0 +1,39 @@
+﻿using HarmonyLib;
+using System;
+using System.Linq;
+using System.Reflection;
+using Verse;
+
+namespace EliteBionicsFrameworkLegacy.Patches
+{
+    internal class EBF_CalloutsRedirect
+    {
+        internal static bool IsCorrectRimWorldVersion()
+        {
+            // only do this for RimWorld 1.3 to 1.5 inclusive
+            // as of 1.6, Callouts (continued by Mlie) has integration with Elite Bionics Framework
+            var version = RimWorldDetector.RimWorldVersion;
+            return version >= new Version(1, 3) && version < new Version(1, 6);
+        }
+
+        public static bool Prepare()
+        {
+            return IsCorrectRimWorldVersion() && TargetMethod() != null;
+        }
+
+        public static MethodBase TargetMethod()
+        {
+            return AccessTools.PropertyGetter("EBF.Util.ModDetector:CalloutsIsLoaded"); ;
+        }
+
+        [HarmonyPostfix]
+        public static void DoNotDetectMlieVariant(ref bool __result)
+        {
+            // do not detect Mlie's variant because they have integrated support for Elite Bionics Framework
+            if (__result)
+            {
+                __result &= !LoadedModManager.RunningMods.Any((ModContentPack pack) => pack.PackageId == "Mlie.Callouts");
+            }
+        }
+    }
+}
